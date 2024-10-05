80563b90d683f4fcdec5f7b6c808df3c6896c0cf
rajkiran426 <razzsingh426@gmail.com>
2024-10-05 20:01:52 +0530

    Added Tailwind CSS and created button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,14 @@
 
 import './App.css';
+import Button from './components/Buttons/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <div>Welcome to React !</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Welcome to React !</div>
+            <Button />
+        </div>
+    );
 }
 
 export default App;
---
+++ my-app/src/components/Buttons/Button.js
@@ -0,0 +1,10 @@
+function Button() {
+    return (
+        <div className="my-2">
+            <button className='bg-blue-400 text-white px-5 hover:bg-blue-700 py-3 rounded-full'>
+                Button 1
+            </button>
+        </div>
+    );
+}
+export default Button; 
