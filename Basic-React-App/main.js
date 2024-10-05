f6420c2ffd219f79cca478dd84c3c9d1f8eb451f
rajkiran426 <razzsingh426@gmail.com>
2024-10-05 14:56:58 +0530

    Babel setup

--- Basic-React-App/main.js
+++ Basic-React-App/main.js
@@ -1,3 +1,14 @@
-const element=<div>Welcome to raj web dev !</div>;
+//const element='<div>Welcome to raj web dev !</div>';
+//const element='<div>Welcome to raj web dev !</div>';
 
-ReactDOM.render(element,document.getElementById('app'));
+const element = React.createElement(
+    'div',
+    {},
+    'Hello World !',
+    React.createElement('h3', { className: "newStyle" }, 'Learning React')
+
+)
+
+const element2 = <div><div>Hello World!</div><h3>Learning React !</h3></div>
+
+ReactDOM.render(element2, document.getElementById('app'));
