50f0f6e8fac3335ef25eb4511b686896e208aaab
rajkiran426 <razzsingh426@gmail.com>
2024-10-05 20:38:06 +0530

    Using class and ES6 created function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Button from './components/Buttons/Button';
+import Main from './components/MainPage/Main';
 function App() {
     return (
         <div className='container mx-auto'>
             <Button />
             <Button />
+
+            <Main />
+
         </div>
     );
 }
--- my-app/src/components/Buttons/Button.js
+++ my-app/src/components/Buttons/Button.js
@@ -1,4 +1,5 @@
-function Button() {
+//function Button() {
+const Button = () => {
     return (
         <div className='my-2'>
             <button className='bg-blue-400 text-white px-5 hover:bg-blue-700 py-3 rounded-full'>
